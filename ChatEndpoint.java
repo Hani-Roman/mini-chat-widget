afb8d499f62e509c119ca587143a4a7c7fd7fa06
Hani <hani-roman@outlook.com>
2025-04-21 13:35:27 +0200

    Create ChatEndpoint.java

---
+++ ChatEndpoint.java
@@ -0,0 +1,36 @@
+package chat;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@ServerEndpoint("/chat")
+public class ChatEndpoint {
+    private static Set<Session> sessions = new CopyOnWriteArraySet<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        sessions.add(session);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session sender) throws IOException {
+        for (Session session : sessions) {
+            if (session.isOpen()) {
+                session.getBasicRemote().sendText(message);
+            }
+        }
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        sessions.remove(session);
+    }
+}
